f8e6241444eba11ee7d49863bbef785383d25c8e
naoketa <49198895+naoketa@users.noreply.github.com>
2019-12-12 23:56:38 +0900

    wip: bit全探索

---
+++ abc147/src/main/scala/c/Main.scala
@@ -0,0 +1,40 @@
+import java.util.Scanner
+
+object Main extends App {
+
+  val sc = new Scanner(System.in)
+  val n = sc.nextInt
+  val input: Array[Map[Int, Int]] = Array.fill(n) {
+    Array.fill(sc.nextInt)(sc.nextInt - 1, sc.nextInt).toMap
+  }
+
+  var result = 0
+  for (i <- 0 until (1 << n)) {
+    val c = f"${i.toBinaryString.toInt}%03d"
+    println(c)
+    var isValid = true
+    import scala.util.control.Breaks.{break, breakable}
+    breakable {
+      for (j <- 0 until n) {
+        print("j人目:" + j)
+        if (((i >> j) & 1) == 1) {
+          println(":親切1")
+          val ok = input(j).forall { pair => c(pair._1).asDigit == pair._2 }
+          println(ok)
+          if (!ok) {
+            isValid = false
+            break
+          }
+        } else {
+          println(":不親切0")
+        }
+      }
+    }
+    if (isValid) {
+      result = Math.max(result, c.count(c => c.equals('1')))
+    }
+
+  }
+
+  println(result)
+}
