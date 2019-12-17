8f954238b44339937860eb9c556ab01b38b74239
naoketa <49198895+naoketa@users.noreply.github.com>
2019-12-18 00:13:30 +0900

    fix: iteratorのサイズ

--- abc145/src/main/scala/c/Main.scala
+++ abc145/src/main/scala/c/Main.scala
@@ -6,7 +6,6 @@ object Main extends App {
   val sc = new Scanner(System.in)
   val n = sc.nextDouble
   val points: Array[(Double, Double)] = Array.fill(n.toInt)((sc.nextDouble, sc.nextDouble))
-  val p = points.permutations
-  val sum: Double = p.map(a => a.sliding(2).map(p => Math.sqrt(Math.pow(p(1)._2 - p(0)._2, 2) + Math.pow(p(1)._1 - p(0)._1, 2))).sum).sum
-  println(sum / p.size)
+  val sum: Double = points.permutations.map(a => a.sliding(2).map(p => Math.sqrt(Math.pow(p(1)._2 - p(0)._2, 2) + Math.pow(p(1)._1 - p(0)._1, 2))).sum).sum
+  println(sum / points.permutations.size)
 }
