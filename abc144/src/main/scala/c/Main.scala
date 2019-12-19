60e74ae8a81bd3a4b3935153eadd75ccbafe6d01
naoketa <49198895+naoketa@users.noreply.github.com>
2019-12-19 22:28:25 +0900

    fix: 最大値をsqrtにする

--- abc144/src/main/scala/c/Main.scala
+++ abc144/src/main/scala/c/Main.scala
@@ -5,9 +5,8 @@ import java.util.Scanner
 object Main extends App {
   val sc = new Scanner(System.in)
   val n = sc.nextLong
-  val max: (Long,Long,Long) = (1L to n / 2L)
-    .collect { case i if n % i == 0 => (i, n / i, i + n / i) }
-    .minBy(_._3)
-  val answer = max._1 + max._2 - 2
+  val answer = (1L to Math.sqrt(n).toLong)
+    .collect { case i if n % i == 0 => i + n / i - 2 }
+    .min
   println(answer)
 }
