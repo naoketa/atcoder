bd6af7b75d9e51d74a085ea31ef1b867d36b8afa
naoketa <49198895+naoketa@users.noreply.github.com>
2020-01-04 00:02:10 +0900

    といた

--- abc141/src/main/scala/a/Main.scala
+++ abc141/src/main/scala/a/Main.scala
@@ -2,30 +2,14 @@ package a
 
 import java.util.Scanner
 
-import scala.collection.mutable
-
 object Main extends App {
   val sc = new Scanner(System.in)
-  val n = sc.nextInt
-  val k = sc.nextLong
-  val q = sc.nextInt
-  var map = mutable.Map.empty[Int, Int]
-  (1 to q).foreach(_ => {
-    val i = sc.nextInt
-    if (map.contains(i)) {
-      map.update(i, map(i) + 1)
-    } else {
-      map.update(i, 1)
-    }
-  })
-
-  (1 to n).map(i => k - q + map.getOrElse(i, 0) > 0)
-    .foreach(isOk => {
-      if (isOk) {
-        println("Yes")
-      } else {
-        println("No")
-      }
-    })
+  val today = sc.next
+  val tomorrow = today match {
+    case "Sunny" => "Cloudy"
+    case "Cloudy" => "Rainy"
+    case "Rainy" => "Sunny"
+  }
+  println(tomorrow)
 
 }
